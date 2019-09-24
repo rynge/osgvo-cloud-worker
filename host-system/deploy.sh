d1f5268d478e561fdf85905f0565c868e296f656
Mats Rynge <mats@rynge.net>
2019-09-24 15:45:25 -0700

    Initial import

---
+++ host-system/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf /opt/osgvo-cloud-worker
+cd /opt
+git clone https://github.com/rynge/osgvo-cloud-worker.git
+
+# install salt
+curl -L https://bootstrap.saltstack.com | bash -
+
+# configure salt for local execution
+echo "file_client: local" >/etc/salt/master.d/50-osg-cloud-worker.conf
+
+salt-call state.highstate
+
+
